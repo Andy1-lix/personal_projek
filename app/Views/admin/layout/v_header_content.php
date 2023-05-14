d9702f2585b096bad47e6aa4726ba6d837668812
= <=>
2023-05-14 08:36:23 +0700

    coba

--- app/Views/admin/layout/v_header_content.php
+++ app/Views/admin/layout/v_header_content.php
@@ -3,13 +3,12 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0"><?= $judul;?></h1>
-        </div>
-        <!-- /.col -->
+          <h1 class="m-0"><?= $judul ?></h1>
+        </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="<?= base_url('admin') ?>/">Home</a></li>
-            <li class="breadcrumb-item active"><?= $judul;?></li>
+            <li class="breadcrumb-item active">Dashboard v1</li>
           </ol>
         </div><!-- /.col -->
       </div><!-- /.row -->
